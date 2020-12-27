803a1edae8e94a1b923aad8adc3df29944e49d48
Andrew Yang <andy.tubeee@gmail.com>
2020-12-27 17:01:53 -0500

    custom routing request args

--- app.py
+++ app.py
@@ -8,7 +8,10 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 @app.route('/jobs', methods=["GET"])
 def welcome():
     if request.method == "GET":
-        data = jobScrape('Android Developer', 'Detroit, MI')
+        jobTitle = request.args['jt']
+        jobLocation = request.args['jl']
+
+        data = jobScrape(jobTitle, jobLocation)
         # print(type(data))
         return data
     else:
